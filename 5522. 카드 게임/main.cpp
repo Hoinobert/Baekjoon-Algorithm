cd0d7f3b2f04ae531209b94c68874573e8cad3e4
DESKTOP-V7SPPHN\ichkn <hoinobert@gmail.com>
2021-04-14 17:16:02 +0900

    add #.5522 using cpp

---
+++ 5522. 카드 게임/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		int score;
+		cin >> score;
+		sum += score;
+	}
+	
+	cout << sum << endl;
+	
+	return 0;
+}
